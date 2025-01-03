272723ecf196149fd2ce48216c6a72ae2da5627d
Robert Babaev <github@robertbabaev.tech>
2025-01-03 16:31:35 -0500

    add dashboard route

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { gadget } from "gadget-server/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [gadget(), react()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
